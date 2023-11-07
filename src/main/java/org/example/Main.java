ac7be35873a168262a79a8df6969ad87b7110492
gewen <gewen54@163.com>
2023-11-07 11:09:37 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i = 1;
+        int j = 2;
+        method();
+    }
+
+    public static void method() {
+        int a = 1;
+    }
+}
